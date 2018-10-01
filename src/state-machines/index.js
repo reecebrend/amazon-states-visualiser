8eef3155f3d85c7167f14a9572fdf1acd07c2b45
Meena.Hoda <meena.hoda@wmfs.net>
2018-10-01 13:37:23 +0100

    refactored the way state machines are retrieved, added a simple calculator example

---
+++ src/state-machines/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  fsa: require('./fsa.json'),
+  simpleTaskMachine: require('./simple-task-machine.json'),
+  simpleCalculator: require('./simple-calculator.json'),
+  help: require('./help.json')
+}
