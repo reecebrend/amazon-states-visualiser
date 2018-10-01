6c5ca253e89680fc2a69a71745e75b6e547a7453
Meena.Hoda <meena.hoda@wmfs.net>
2018-10-01 13:50:28 +0100

    added some more examples wait and parallel

--- src/state-machines/index.js
+++ src/state-machines/index.js
@@ -2,5 +2,7 @@ module.exports = {
   fsa: require('./fsa.json'),
   simpleTaskMachine: require('./simple-task-machine.json'),
   simpleCalculator: require('./simple-calculator.json'),
-  help: require('./help.json')
+  help: require('./help.json'),
+  wait: require('./wait.json'),
+  parallel: require('./parallel.json')
 }
